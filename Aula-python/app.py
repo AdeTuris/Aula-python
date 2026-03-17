7e06d833b025332f6ee538f15c6d07816ca5463c
AdeTuris <166664392+AdeTuris@users.noreply.github.com>
2026-03-16 22:21:17 -0300

    Add files via upload

---
+++ Aula-python/app.py
@@ -0,0 +1,19 @@
+//idade = 20
+
+if idade >= 18:
+    print("Maior de idade")
+
+    dade =
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+
+    if nota >= 7:
+    print("Aprovado")
+elif nota >= 4 and nota <= 6:
+    print("Recuperação")
+else:
+    print("Reprovado")
